76a337b06ff41c98b981009bd0192335205ef804
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2020-11-02 18:05:57 +0300

    Updated for 0.0.10

--- src/main/kotlin/LeNetMnist.kt
+++ src/main/kotlin/LeNetMnist.kt
@@ -1,22 +1,21 @@
 
-import api.core.Sequential
-import api.core.WritingMode
-import api.core.activation.Activations
-import api.core.initializer.Constant
-import api.core.initializer.GlorotNormal
-import api.core.initializer.Zeros
-import api.core.layer.Dense
-import api.core.layer.Flatten
-import api.core.layer.Input
-import api.core.layer.twodim.Conv2D
-import api.core.layer.twodim.ConvPadding
-import api.core.layer.twodim.MaxPool2D
-import api.core.loss.Losses
-import api.core.metric.Metrics
-import api.core.optimizer.Adam
-import api.inference.InferenceModel
-import datasets.Dataset
-import datasets.handlers.*
+import org.jetbrains.kotlinx.dl.api.core.Sequential
+import org.jetbrains.kotlinx.dl.api.core.WritingMode
+import org.jetbrains.kotlinx.dl.api.core.activation.Activations
+import org.jetbrains.kotlinx.dl.api.core.initializer.Constant
+import org.jetbrains.kotlinx.dl.api.core.initializer.GlorotNormal
+import org.jetbrains.kotlinx.dl.api.core.initializer.Zeros
+import org.jetbrains.kotlinx.dl.api.core.layer.Dense
+import org.jetbrains.kotlinx.dl.api.core.layer.Flatten
+import org.jetbrains.kotlinx.dl.api.core.layer.Input
+import org.jetbrains.kotlinx.dl.api.core.layer.twodim.Conv2D
+import org.jetbrains.kotlinx.dl.api.core.layer.twodim.ConvPadding
+import org.jetbrains.kotlinx.dl.api.core.layer.twodim.MaxPool2D
+import org.jetbrains.kotlinx.dl.api.core.loss.Losses
+import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
+import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
+import org.jetbrains.kotlinx.dl.datasets.Dataset
+import org.jetbrains.kotlinx.dl.datasets.handlers.*
 import java.io.File
 
 
