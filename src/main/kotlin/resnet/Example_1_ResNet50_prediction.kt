c9cfae85600c700f9a65b76446c2121ac9ec341b
Alexey Zinoviev <zaleslaw.sin@gmail.com>
2021-09-02 19:48:38 +0300

    Added support for 0.3.0-alpha-1 version

--- src/main/kotlin/resnet/Example_1_ResNet50_prediction.kt
+++ src/main/kotlin/resnet/Example_1_ResNet50_prediction.kt
@@ -10,9 +10,9 @@ import org.jetbrains.kotlinx.dl.api.core.loss.Losses
 import org.jetbrains.kotlinx.dl.api.core.metric.Metrics
 import org.jetbrains.kotlinx.dl.api.core.optimizer.Adam
 import org.jetbrains.kotlinx.dl.api.inference.keras.loadWeights
-import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.ModelType
-import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.ModelZoo
-import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.predictTop5Labels
+import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.TFModels
+import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.TFModelHub
+import org.jetbrains.kotlinx.dl.api.inference.keras.loaders.predictTop5ImageNetLabels
 import org.jetbrains.kotlinx.dl.dataset.image.ColorOrder
 import org.jetbrains.kotlinx.dl.dataset.preprocessor.ImageShape
 import org.jetbrains.kotlinx.dl.dataset.preprocessor.Preprocessing
@@ -25,19 +25,19 @@ import java.net.URL
 
 /**
  * This examples demonstrates the inference concept on ResNet'50 model:
- * - Model configuration, model weights and labels are obtained from [ModelZoo].
+ * - Model configuration, model weights and labels are obtained from [TFModelHub].
  * - Weights are loaded from .h5 file, configuration is loaded from .json file.
  * - Model predicts on a few images located in resources.
  * - No additional training.
  * - No new layers are added.
- * - Special preprocessing (used in MobileNetV2 during training on ImageNet dataset) is applied to images before prediction.
+ * - Special preprocessing (used in ResNet'50 during training on ImageNet dataset) is applied to images before prediction.
  */
 fun resnet50prediction() {
-    val modelZoo =
-        ModelZoo(commonModelDirectory = File("cache/pretrainedModels"), modelType = ModelType.ResNet_50)
-    val model = modelZoo.loadModel() as Functional
+    val modelHub =
+        TFModelHub(commonModelDirectory = File("cache/pretrainedModels"), modelType = TFModels.CV.ResNet_50)
+    val model = modelHub.loadModel() as Functional
 
-    val imageNetClassLabels = modelZoo.loadClassLabels()
+    val imageNetClassLabels = modelHub.loadClassLabels()
 
     model.use {
         it.compile(
@@ -48,7 +48,7 @@ fun resnet50prediction() {
 
         it.summary()
 
-        val hdfFile = modelZoo.loadWeights()
+        val hdfFile = modelHub.loadWeights()
 
         it.loadWeights(hdfFile)
 
@@ -63,12 +63,12 @@ fun resnet50prediction() {
                 }
             }
 
-            val inputData = modelZoo.preprocessInput(preprocessing().first, model.inputDimensions)
+            val inputData = modelHub.preprocessInput(preprocessing().first, model.inputDimensions)
 
             val res = it.predict(inputData)
             println("Predicted object for image$i.jpg is ${imageNetClassLabels[res]}")
 
-            val top5 = predictTop5Labels(it, inputData, imageNetClassLabels)
+            val top5 = predictTop5ImageNetLabels(it, inputData, imageNetClassLabels)
 
             println(top5.toString())
         }
@@ -77,6 +77,7 @@ fun resnet50prediction() {
 
 /** */
 fun main(): Unit = resnet50prediction()
+
 
 /** Converts resource string path to the file. */
 @Throws(URISyntaxException::class)
